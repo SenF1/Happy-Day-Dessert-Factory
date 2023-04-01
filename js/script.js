6547970278b7aff815d8aad422fe91a99b6de513
Sen Feng <senfeng6@gmail.com>
2023-04-01 16:28:47 -0400

    modify description of menu

--- js/script.js
+++ js/script.js
@@ -44,16 +44,3 @@ function toggleDropdown(event) {
         dropdownContent.classList.toggle("show");
     }
 }
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-if (!event.target.matches('.dropdown a')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-        }
-      }
-    }
-}
